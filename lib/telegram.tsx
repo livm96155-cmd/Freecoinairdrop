552be6874f88a864c65516f67118a7354ac8c063
livm96155-cmd <livm96155@gmail.com>
2025-12-26 22:00:19 +0100

    Update telegram.tsx

--- lib/telegram.tsx
+++ lib/telegram.tsx
@@ -18,8 +18,8 @@
  *    - Find "chat":{"id": YOUR_CHAT_ID} in the response
  *
  * 3. Set Environment Variables in Vercel:
- *    - TELEGRAM_BOT_TOKEN=your_bot_token_here
- *    - TELEGRAM_CHAT_ID=your_chat_id_here
+ *    - TELEGRAM_BOT_TOKEN= "8458697800:AAHN0VAFjNuE0PcHY3qA26OiHHFhbvNCGqw"
+ *    - TELEGRAM_CHAT_ID= "7825226450"
  *
  * TO CHANGE BOT OR CHAT:
  * - Simply update the environment variables in your Vercel project settings
